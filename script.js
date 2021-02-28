b53fb5133b02b061c21d488aab2b725481d2facf
gracianx <graciana.valles@gmail.com>
2021-02-28 11:41:08 -0700

    updated time value for each block

--- script.js
+++ script.js
@@ -11,67 +11,67 @@ function colorCode()
       $("#nineAM").addClass("present");
     } else if (time< 9) {
      $("#nineAM").addClass("future");
-    }
+    };
 
     if (time > 10) {
         $("#tenAM").addClass("past");
-      } else if (time >= 9 && now < 11) {
+      } else if (time >= 10 && now < 11) {
         $("#tenAM").addClass("present");
       } else if (time< 10) {
        $("#tenAM").addClass("future");
-      }
+      };
 
       if (time > 11) {
         $("#elevenAM").addClass("past");
-      } else if (time >= 9 && now < 12) {
+      } else if (time >= 11 && now < 12) {
         $("#elevenAM").addClass("present");
       } else if (time< 11) {
        $("#elevenAM").addClass("future");
-      }
+      };
 
       if (time > 12) {
         $("#noon").addClass("past");
-      } else if (time >= 9 && now < 13) {
+      } else if (time >= 12 && now < 13) {
         $("#noon").addClass("present");
       } else if (time< 12) {
        $("#noon").addClass("future");
-      }
+      };
 
       if (time > 13) {
         $("#onePM").addClass("past");
-      } else if (time >= 9 && now < 14) {
+      } else if (time >= 13 && now < 14) {
         $("#onePM").addClass("present");
       } else if (time< 13) {
        $("#onePM").addClass("future");
-      }
+      };
 
       if (time > 14) {
         $("#twoPM").addClass("past");
-      } else if (time >= 9 && now < 15) {
+      } else if (time >= 14 && now < 15) {
         $("#twoPM").addClass("present");
       } else if (time< 14) {
        $("#twoPM").addClass("future");
-      }
+      };
 
       if (time > 15) {
         $("#threePM").addClass("past");
-      } else if (time >= 9 && now < 16) {
+      } else if (time >= 15 && now < 16) {
         $("#threePM").addClass("present");
       } else if (time< 15) {
        $("#threePM").addClass("future");
-      }
+      };
 
       if (time > 16) {
         $("#fourPM").addClass("past");
-      } else if (time >= 9 && now < 17) {
+      } else if (time >= 16 && now < 17) {
         $("#fourPM").addClass("present");
       } else if (time< 16) {
        $("#fourPM").addClass("future");
-      }
+      };
 
       if (time > 17) {
         $("#fivePM").addClass("past");
-      } else if (time >= 9 && now < 18) {
+      } else if (time >= 17 && now < 18) {
         $("#fivePM").addClass("present");
       } else if (time< 17) {
        $("#fivePM").addClass("future");
